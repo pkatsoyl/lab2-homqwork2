8049d3589fe3402767689ccb959e072348070595
Paul Katsoulas <pkatsoyl@gmail.com>
2023-10-29 23:22:25 +0200

    final commit

--- lab2-homqwork2/grammar.flex.cpp
+++ lab2-homqwork2/grammar.flex.cpp
@@ -355,8 +355,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 1
-#define YY_END_OF_BUFFER 2
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -364,9 +364,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[6] =
+static const flex_int16_t yy_accept[12] =
     {   0,
-        0,    0,    2,    1,    0
+        2,    2,    4,    3,    2,    1,    2,    2,    2,    1,
+        0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -375,16 +376,16 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    2,    3,    3,
+        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    4,    4,    4,    4,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -401,29 +402,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[2] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1
+        1,    2,    2,    2
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[14] =
     {   0,
-        0,    0,    2,    3,    3,    0
+        0,    0,   11,   12,    6,    5,    4,    0,    0,    3,
+       12,    4,    3
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[14] =
     {   0,
-        6,    6,    5,    5,    0,    5
+       11,    1,   11,   11,   12,   13,   12,    7,    7,   13,
+        0,   11,   11
     } ;
 
-static const flex_int16_t yy_nxt[5] =
+static const flex_int16_t yy_nxt[17] =
     {   0,
-        4,    5,    3,    5
+        4,    5,    6,    7,   10,    8,    9,    9,    9,    9,
+       11,    3,   11,   11,   11,   11
     } ;
 
-static const flex_int16_t yy_chk[5] =
+static const flex_int16_t yy_chk[17] =
     {   0,
-        6,    3,    5,    5
+        1,    1,    1,    1,   13,   12,   10,    7,    6,    5,
+        3,   11,   11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,7 +446,7 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "grammar.l"
-#line 444 "grammar.flex.cpp"
+#line 449 "grammar.flex.cpp"
 
 #define INITIAL 0
 
@@ -658,10 +663,7 @@ YY_DECL
 #line 4 "grammar.l"
 
 
-
-
-
-#line 664 "grammar.flex.cpp"
+#line 666 "grammar.flex.cpp"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -688,13 +690,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 6 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 3 );
+		while ( yy_base[yy_current_state] != 12 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -720,10 +722,20 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
+#line 6 "grammar.l"
+{ printf("the integer is :%s\n", yytext); }
+	YY_BREAK
+case 2:
+YY_RULE_SETUP
 #line 9 "grammar.l"
+{ printf("the string is :%s\n", yytext); }
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 12 "grammar.l"
 ECHO;
 	YY_BREAK
-#line 726 "grammar.flex.cpp"
+#line 738 "grammar.flex.cpp"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1020,7 +1032,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 6 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1048,11 +1060,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 6 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 5);
+	yy_is_jam = (yy_current_state == 11);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1728,7 +1740,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 9 "grammar.l"
+#line 12 "grammar.l"
 
 
 
--- lab2-homqwork2/main.cpp
+++ lab2-homqwork2/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+extern FILE* yyin;
+extern int yylex();
 
 int main(int argc, char* argv[]) {
 	
-
+	fopen_s(&yyin, "textfile.txt", "r");
+	yylex();
 	return 0;
 }
